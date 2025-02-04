e2d70f25b578cb8acab48ff17addf798c4202649
vidhyasai <relangividhyasai@gmail.com>
2025-02-04 13:07:56 +0530

    datasource/locals.tf

--- datasource/locals.tf
+++ datasource/locals.tf
@@ -1,4 +1,5 @@
 locals {
-    ami_id = data.aws_ami.join_devops.id
-    instance_type = "t3.micro"
+  ami_id = data.aws_ami.joindevops.id
+  instance_type = "t3.micro"
+  tag_name = "${var.project}-${var.component}-${var.environment}"
 }
