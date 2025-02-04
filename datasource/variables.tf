fc84986ea62d428ce594cd165f787b4f419d4af0
vidhyasai <relangividhyasai@gmail.com>
2025-02-04 13:09:41 +0530

    datasource/variables.tf

---
+++ datasource/variables.tf
@@ -0,0 +1,14 @@
+variable  "project" {
+  type = string
+  default = "expense"
+}
+
+variable "component" {
+    type = string
+    default = "backend"
+}
+
+variable "environment" {
+    type = string
+    default = "dev"
+}
