a18078ccbac6bd1acf45012ac5df7badb472e10e
vidhyasai <relangividhyasai@gmail.com>
2025-02-05 14:14:54 +0530

    terraform-multiple-env/tfvars/r53.tf

--- terraform-multiple-env/tfvars/r53.tf
+++ terraform-multiple-env/tfvars/r53.tf
@@ -1,19 +1,9 @@
-/* resource "aws_route53_record" "expense" {
+resource "aws_route53_record" "expense" {
     count = length(var.instance)
     zone_id = var.zone_id
-    name    = "${var.instance[count.index]}.${var.domain_name}"
+    name    = var.instance[count.index] == "frontend" && var.environment == "prod" ? var.domain_name : "${var.instance[count.index]}-${var.environment}.${var.domain_name}"
     type    = "A"
     ttl     = 1
-    records = [aws_instance.expense[count.index].private_ip]
+    records = [var.instance[count.index] == "frontend" && var.environment == "prod" ? aws_instance.expense[count.index].public_ip : aws_instance.expense[count.index].private_ip]
     allow_overwrite = true
 }
-
-resource "aws_route53_record" "frontend" {
-    count = 3
-    zone_id = var.zone_id
-    name    = "${var.domain_name}"
-    type    = "A"
-    ttl     = 1
-    records = [aws_instance.expense[2].public_ip]
-    allow_overwrite = true
-} */
